e1e08de8d8e68f2704f03aeda37e4e688115231f
Raunaq Singh <raunaq.singh2007@gmail.com>
2026-08-25 12:53:53 +0530

    set

--- sets.py
+++ sets.py
@@ -1,1 +1,24 @@
-set = {1, 2, 3}
+set1 = {"apple", "banana", "orange"}
+list1 = {4, 5, 6}
+set2 = {1, 2, 3}
+for i in set1:
+    print(i)
+
+if "banana" in set1:
+    print("Yes")
+
+set1.add("kiwi")
+print(set1)
+set1.update(set2)
+print(set1)
+set1.update(list1)
+print(set1)
+
+set1.remove(1)
+print(set1)
+
+for i in set1:
+    print(i)
+
+myset = set1.union(set2)
+print(myset)
